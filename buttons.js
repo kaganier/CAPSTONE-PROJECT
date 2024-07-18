6b5119bb924e32a3c33642e72ed7a7bd90d31c1f
kabrini <kgganier@gmail.com>
2024-07-18 02:30:17 -0500

    update nav buttons

--- buttons.js
+++ buttons.js
@@ -1,12 +1,25 @@
-function myScriptureChanger () {
-   
-    let scripture;
-    let time;
+function myScriptureChanger() {
+  let scripture;
+  let time;
 
-    scripture = prompt("Whats your favorite scripture that helps with your mental health crisis?");
-    time = prompt("what is your favorite timr to pray?");
-myScriptureChangerHeader.innerHTML = (`Hello accordingto you ${scripture} is your favorite buble verse!`);
-myScriptureChsngerParagraph.innerHTML = (`${time} is what you picked as your favorite prayer time keep it up!`);
+  scripture = prompt(
+    "Whats your favorite scripture that helps with your mental health crisis?"
+  );
+  time = prompt("what is your favorite time to pray?");
+  myScriptureChangerHeader.innerHTML = `Hello according to you ${scripture} is your favorite buble verse!`;
+  myScriptureChsngerParagraph.innerHTML = `${time} is what you picked as your favorite prayer time keep it up!`;
 }
 
 myScriptureChangerButton.onclick = myScriptureChanger;
+
+function myMentalHelperChanger() {
+  let coping;
+  let syles;
+
+  coping = prompt("How often do you utilize mental health coping mechanisms");
+  styles = prompt("What are some of your coping styles?");
+
+  myMentalHelperChangerButton.onClick = myMentalHelperChanger;
+
+}
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
